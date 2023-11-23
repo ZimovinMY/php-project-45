5085b23403a05071bba5c6d2985a0acd1a4253ad
ZimovinMY <misch.zimowin2013@mail.ru>
2023-11-23 16:59:19 +0300

    part 9

---
+++ src/Games/Prime.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+
+use function BrainGames\Engine\gameInteraction;
+
+function startPrimeGame()
+{
+    $gameGreeting = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    for ($i = 0; $i < 3; $i++) {
+	$question = rand(1, 100);
+	$answer = 'yes';
+	for ($divisor = 2; $divisor <= $question / 2; $divisor++) {
+            if ($question % $divisor === 0) {
+		$answer = 'no';
+	    }
+	}
+        $gameData[$question] = $answer;
+    };
+    gameInteraction($gameGreeting, $gameData);
+}
