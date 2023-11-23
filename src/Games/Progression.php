4cc5d3d655ac09c13d6104489624b83598e3cb6c
ZimovinMY <misch.zimowin2013@mail.ru>
2023-11-23 16:28:17 +0300

    part 8

---
+++ src/Games/Progression.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BrainGames\Games\Progression;
+
+use function BrainGames\Engine\gameInteraction;
+
+function startProgressionGame()
+{
+    $gameGreeting = 'What number is missing in the progression?';
+    for ($i = 0; $i < 3; $i++) {
+        $begin = rand(0, 50);
+        $step = rand(1, 5);
+	$end = $begin + 10 * $step;
+	$posMissingNumber = rand(0,9);
+	$progression = range($begin, $end, $step);
+	$answer = $progression[$posMissingNumber];
+	$progression[$posMissingNumber] = '..';
+        $question = implode(' ', $progression);
+        $gameData[$question] = $answer;
+    };
+    gameInteraction($gameGreeting, $gameData);
+}
