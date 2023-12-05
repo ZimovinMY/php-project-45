9067dbb0fbef84a81c0c49e70b0ffb516c279d93
ZimovinMY <misch.zimowin2013@mail.ru>
2023-12-05 23:44:53 +0300

    fix remarks v.3

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -14,13 +14,27 @@ function start(): void
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $begin = rand(0, 50);
         $step = rand(1, 5);
-        $end = $begin + 9 * $step;
         $posMissingNumber = rand(0, 9);
-        $progression = range($begin, $end, $step);
-        $answer = $progression[$posMissingNumber];
-        $progression[$posMissingNumber] = '..';
+        $progression = getProgression($begin, $step, $posMissingNumber);
+        $answer = match ($posMissingNumber) {
+            0 => $progression[$posMissingNumber + 1] - $step,
+            default => $progression[$posMissingNumber - 1] + $step,
+        };
         $question = implode(' ', $progression);
         $gameData[$question] = $answer;
     };
     gameInteraction(GREETING_TEXT, $gameData);
+}
+
+function getProgression(int $begin, int $step, int $posMissingNumber): array
+{
+    $progression = [];
+    for ($i = 0; $i <= 9; $i += 1) {
+        if ($i === $posMissingNumber) {
+            $progression[] = '..';
+        } else {
+            $progression[] = $begin + $step * $i;
+        }
+    }
+    return $progression;
 }
